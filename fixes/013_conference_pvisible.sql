845035858c00a9527ac4710fbf13459bb53af077
Andreas Kupries <akupries@shaw.ca>
2016-02-06 07:43:00 +0000

    Added management for visibility of the conference proceedings, and the proper proceedings page. Extended and updated testsuite. Database fixup.

---
+++ fixes/013_conference_pvisible.sql
@@ -0,0 +1,83 @@
+-- - - -- --- ----- -------- ------------- ---------------------
+-- New column	"conference.pvisible", plus helper table for human-readable labeling.
+-- 
+-- Default to insert: 1 <=> pending
+
+-- ---------------------------------------------------------------
+CREATE TABLE pvisible (	-- fixed content
+	id		INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+	text		TEXT	NOT NULL UNIQUE
+);
+INSERT OR IGNORE INTO pvisible VALUES (1,'hidden');
+INSERT OR IGNORE INTO pvisible VALUES (2,'visible');
+-- --------------------------------------------------------------
+
+CREATE TABLE new_conference (
+	    id		INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+	    title	TEXT	NOT NULL UNIQUE,
+	    year	INTEGER NOT NULL,
+
+	    management	INTEGER	NOT NULL REFERENCES contact,	-- Org/company/person managing the conference
+	    submission	INTEGER	NOT NULL REFERENCES email,	-- Email to receive submissions.
+
+	    city	INTEGER REFERENCES city,
+	    hotel	INTEGER REFERENCES hotel, -- We do not immediately know where we will be
+	    facility	INTEGER REFERENCES hotel, -- While sessions are usually at the hotel, they may not be.
+
+	    startdate	INTEGER,		-- [*], date [epoch]
+	    enddate	INTEGER,		--	date [epoch]
+	    alignment	INTEGER NOT NULL,	-- iso8601 weekday (1:mon...7:sun), or -1 (no alignment)
+	    length	INTEGER NOT NULL,	-- length in days
+
+	    talklength	INTEGER NOT NULL,	-- minutes	  here we configure
+	    sessionlen	INTEGER NOT NULL,	-- in #talks max  basic scheduling parameters.
+						-- 		  shorter talks => longer sessions.
+						-- 		  standard: 30 min x3
+	    rstatus	INTEGER NOT NULL REFERENCES rstatus,
+	    pvisible	INTEGER NOT NULL REFERENCES pvisible,
+
+	    pschedule   INTEGER REFERENCES pschedule
+
+	    -- Constraints:
+	    -- future expansion columns:
+	    -- -- max day|range for tutorials
+	    -- -- max number of tracks for tutorials
+	    -- -- max number of tracks for sessions
+
+	    -- Constraints:
+	    -- * (city == facility->city) WHERE facility IS NOT NULL
+	    -- * (city == hotel->city)    WHERE facility IS NULL AND hotel IS NOT NULL
+	    --   Note: This covers the possibility of hotel->city != session->city
+	    --   In that case we expect the conference to be in the city where the sessions are.
+	    --
+	    -- * year      == year-of    (start-date)
+	    -- * alignment == weekday-of (start-date) WHERE alignment > 0.
+	    -- * enddate   == startdate + length days
+
+	    -- [Ad *] from this we can compute a basic timeline
+	    --	for deadlines and actions (cfp's, submission
+	    --	deadline, material deadline, etc)
+	    --	Should possibly save it in a table, and allow
+	    --	for conversion into ical and other calender formats.
+	    --
+	    -->	Google Calendar of the Conference, Mgmt + Public
+)
+;
+
+-- Copy data
+INSERT
+INTO	new_conference
+SELECT	id, title, year, management, submission, city, hotel, facility,
+	startdate, enddate, alignment, length, talklength, sessionlen,
+	rstatus, 1, pschedule
+FROM conference
+;
+
+-- Switch things around
+DROP TABLE conference
+;
+ALTER TABLE new_conference RENAME TO conference
+;
+
+-- Done
+-- - - -- --- ----- -------- ------------- ---------------------
